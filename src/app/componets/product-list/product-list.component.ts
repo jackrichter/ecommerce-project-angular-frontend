da3f62fbebbd26afed51fa0318b5b0eff7a29e31
jackrichter <jackrichter1@gmail.com>
2023-03-24 19:00:20 +0100

    Ver 2.0: Enhanced to read category id parameter.

--- src/app/componets/product-list/product-list.component.ts
+++ src/app/componets/product-list/product-list.component.ts
@@ -1,26 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/common/product';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list-grid.component.html',
-//  templateUrl: './product-list-table.component.html',
-//  templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit{
+export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
+  currentCategoryId: number = 1;
 
-  constructor(private productService: ProductService) {}
+  constructor(private productService: ProductService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.getListProducts();
+
+    this.route.paramMap.subscribe(() => {
+      this.getListProducts();
+    });
+
   }
 
   getListProducts() {
-    this.productService.getProductList().subscribe(data => this.products = data);
-  }
 
+    // Check if parameter 'id' is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    // Get the 'id' param which is a string, and convert it to a nummber, using the "+" symbol
+    // Make use of the NON-NULL Assertion Operator "!"
+    if (hasCategoryId) {
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }
+    else {
+      // default to category id 1
+      this.currentCategoryId = 1;
+    }
+
+    // Get the products for the given category id
+    this.productService.getProductList(this.currentCategoryId).subscribe(data => this.products = data);
+  }
 }

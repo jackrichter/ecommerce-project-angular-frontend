421c5c9722a708ea6112f1b6ff0713b5e4d07dc2
jackrichter <jackrichter1@gmail.com>
2023-03-25 13:49:23 +0100

    Ver 2.0: Updated code: find products by categoryId

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -15,9 +15,10 @@ export class ProductService {
 
   getProductList(theCategoryId: number): Observable<Product[]> {
 
-    // @TODO: need to build url based on category id ... backend REST Api
+    // Build url based on category id ... backend REST Api
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
       map(response => response._embedded.products)
     );
   }
